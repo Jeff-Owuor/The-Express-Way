38028c0da1b9bb5885172e814273e67af9a24d05
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-07-07 15:32:46 +0300

    Create phonebook model which represents the document to be saved by mongoDb
    Connect to mongoDb
    create a schema
    export the model so that it can be used in other files

---
+++ models/Phonebook.js
@@ -0,0 +1,23 @@
+const mongoose  = require("mongoose")
+require('dotenv').config()
+
+const url = process.env.MONGODB_URI
+mongoose.set('strictQuery',false)
+mongoose.connect(url).then((result)=>{
+    console.log('Connected successfully to MongoDb!')
+})
+.catch((error)=>{
+ console.log(`Error occurred when connecting to MongoDb ${error.message}`)
+})
+const phoneBookSchema = mongoose.Schema({
+    name:String,
+    number:String
+})
+
+phoneBookSchema.set("toJSON",{transform: (document,returnedObj)=>{
+        returnedObj.id = returnedObj._id.toString();
+        delete returnedObj._id;
+        delete returnedObj.__v;
+}})
+
+module.exports = mongoose.model("Phonebook",phoneBookSchema)
